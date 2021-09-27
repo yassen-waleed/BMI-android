92496fac6a91a9e93b8ebbacb2b93d0215b98cf7
yassen-waleed <81925932+yassen-waleed@users.noreply.github.com>
2021-09-27 14:36:43 +0300

    v2

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 private EditText editheight ;
 private EditText editweight ;
 private TextView result ;
+    private TextView type ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +21,43 @@ private TextView result ;
         editheight = findViewById(R.id.editheight);
         editweight = findViewById(R.id.editweight);
         result =findViewById(R.id.result) ;
+        type =findViewById(R.id.type);
 
     }
-    public void calculateBMI(View view){
-        double weight = Double.parseDouble(editweight.getText().toString()) ;
-        double height = Double.parseDouble(editheight.getText().toString()) ;
-        double bmi = weight/Math.pow((height/100),2) ;
-        String str="BMI is "+bmi ;
-        result.setText(str);
-        Toast.makeText(this,str,Toast.LENGTH_SHORT).show();
+
+   public void calculateBMI(View view){
+       if(isNumeric(editweight.getText().toString()) && isNumeric(editweight.getText().toString())) {
+           double weight = Double.parseDouble(editweight.getText().toString());
+           double height = Double.parseDouble(editheight.getText().toString());
+           double bmi = weight / Math.pow((height / 100), 2);
+           String str = "BMI is " + bmi;
+           result.setText(str);
+           Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+           if (bmi < 18.5) {
+               type.setText("Underweight");
+
+           } else if (bmi >= 18.5 && bmi < 25) {
+               type.setText("Normal Weight");
+           } else if (bmi >= 25 && bmi < 30) {
+               type.setText("Overweight");
+           } else {
+               type.setText("Obese");
+
+           }
+       }else {
+           result.setText("please enter a number");
+       }
+
+   }
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
